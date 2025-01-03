0ca803987978f7181fcad06908c9a707d9a23445
Heath Babb <heath@heath-babb.tech>
2025-01-02 21:39:03 -0500

    feat: integrate Sentry for error monitoring and add new components

--- src/env/client.ts
+++ src/env/client.ts
@@ -4,8 +4,10 @@ import { z } from "zod";
 export const env = createEnv({
   client: {
     NEXT_PUBLIC_API_URL: z.string().url().default("http://localhost:3000/api"),
+    NEXT_PUBLIC_BASE_URL: z.string().url().default("http://localhost:3000"),
   },
   runtimeEnv: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   },
 });
